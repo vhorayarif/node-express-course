152349404bb8240e2f13064dcbea39ab377d6892
Yarif Vhora <vhorayarif@gmail.com>
2021-04-03 21:34:25 -0500

    add login Post route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData=[
   {name:'Mark'},
   {name:'Jill'}
@@ -12,7 +15,7 @@ app.get('/users',function(req,res){
     users: mockUserData
   })
 })
-
+//colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
   console.log(req.params.id)
   res.json({
@@ -20,6 +23,27 @@ app.get('/users/:id',function(req,res){
     message: 'got one user',
     user: req.params.id
   })
+})
+
+app.post('/login',function(req,res){
+  const username=req.body.username;
+  const password=req.body.password;
+
+  const mockUsername="billyTheKid";
+  const mockPassword="superSecret";
+
+  if(username===mockUsername && password===mockPassword){
+    res.json({
+      sucess: true,
+      message: 'password and username match!',
+      token: 'encrypted token goes here'
+    })
+  } else {
+    res.json({
+      sucess: false,
+      message: 'passoword and username do not match'
+    })
+  }
 })
 
 
